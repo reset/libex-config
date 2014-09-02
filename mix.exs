98ac74133547e4b533345b0c4c8ac76f19664416
Jamie Winsor <jamie@vialstudios.com>
2014-09-02 11:58:32 -0700

    update elixir constraint for 0.15.x and 1.0.0-rc1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule LibEx.Config.Mixfile do
   def project do
     [app: :libex_config,
      version: "0.1.1",
-     elixir: "~> 0.12",
+     elixir: "~> 0.15.0 or ~> 1.0.0-rc1",
      description: description,
      package: package,
      deps: deps]
